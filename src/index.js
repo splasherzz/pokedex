0116a44d172c7c5de8fb2b2295e7c48aca5c4b85
splasherzz <juliasabado30@gmail.com>
2024-07-13 22:34:34 +0800

    feat: add filter system

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ const API_BASE = "https://pokeapi.co/api/v2/"
 const OFFSET_AMOUNT = 10;
 
 let currentOffset = 0;
+const pokemonDataList = [];
 
 const cardList = document.getElementById('cardlist');
 const loadMoreBtn = document.getElementById('loadmore');
+const filterForm = document.getElementById('filterform')
 
 async function getPokemonDetailsByURL(url) {
     try {
@@ -38,12 +40,15 @@ async function getPokemons(amount, offset) {
     }
 }
 
-async function loadMorePokemons() {
-    const pokemons = await getPokemons(OFFSET_AMOUNT, currentOffset);
-    // loop over the results, for each result, perform fetch to get the details
-    for(const pokemon of pokemons) {
-        const pokemonDetails = await getPokemonDetailsByURL(pokemon.url);
+function loadPokemons(amount, offset) {
+    console.log(offset)
+    return pokemonDataList.slice(offset, offset+amount);
+}
 
+async function loadMorePokemons() {
+    const pokemons = loadPokemons(OFFSET_AMOUNT, currentOffset);
+    // loop over the results, for each result, perform fetch to get the details
+    for(const pokemonDetails of pokemons) {
         const id = pokemonDetails.id;
         const name = pokemonDetails.name;
         const types = pokemonDetails.types.map(type => type.type.name).join(', ');
@@ -56,15 +61,73 @@ async function loadMorePokemons() {
             <div>${name}</div>
             <div>Type(s): ${types}</div>`
 
-        console.log(newCard);
         cardList.appendChild(newCard);
     }
 
     // Update global state
     currentOffset = currentOffset + OFFSET_AMOUNT;
 }
-loadMoreBtn.addEventListener('click', loadMorePokemons)
+
+function applyFilter(id, name) {
+    const filteredPokemon = pokemonDataList.filter(pokemon => {
+        return pokemon.id == id || pokemon.name === name || id === '' && name === '';
+    });
+
+    console.log(id, name)
+    console.log(filteredPokemon)
+
+    cardList.innerHTML = '';
+    filteredPokemon.forEach(pokemonDetails => {
+        const id = pokemonDetails.id;
+        const name = pokemonDetails.name;
+        const types = pokemonDetails.types.map(type => type.type.name).join(', ');
+        const imgSrc = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id.toString().padStart(3, '0')}.png`;
+
+        const newCard = document.createElement("div");
+        newCard.setAttribute("class", "card")
+        newCard.innerHTML = `<img src="${imgSrc}" alt="${name}">
+            <div>Id No. ${id.toString().padStart(3, '0')}</div>
+            <div>${name}</div>
+            <div>Type(s): ${types}</div>`
+
+        cardList.appendChild(newCard);
+    })
+}
+
+loadMoreBtn.addEventListener('click', loadMorePokemons);
+
+filterForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const id = document.getElementById('id').value;
+    const name = document.getElementById('name').value;
+    applyFilter(id, name);
+});
 
 window.onload = () => {
-    loadMorePokemons();
+    getPokemons(1_010, 0)
+        .then(async (pokemons) => {
+            let numProcessed = 0;
+            for(const pokemon of pokemons) {
+                const pokemonDetails = await getPokemonDetailsByURL(pokemon.url);
+                pokemonDataList.push(pokemonDetails);
+                console.log(pokemon);
+                
+                if (currentOffset < OFFSET_AMOUNT) {
+                    const id = pokemonDetails.id;
+                    const name = pokemonDetails.name;
+                    const types = pokemonDetails.types.map(type => type.type.name).join(', ');
+                    const imgSrc = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id.toString().padStart(3, '0')}.png`;
+    
+                    const newCard = document.createElement("div");
+                    newCard.setAttribute("class", "card")
+                    newCard.innerHTML = `<img src="${imgSrc}" alt="${name}">
+                        <div>Id No. ${id.toString().padStart(3, '0')}</div>
+                        <div>${name}</div>
+                        <div>Type(s): ${types}</div>`
+    
+                    cardList.appendChild(newCard);
+                    currentOffset += 1;
+                }
+            }
+    });
 }
